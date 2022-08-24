16d1b97f8bc544bf7f47046bb3da9d7e7413336a
jusang53 <98146496+jusang53@users.noreply.github.com>
2022-08-25 01:08:07 +0900

    Update Plusscript.m

--- Plusscript.m
+++ Plusscript.m
@@ -1,20 +1,19 @@
 classdef Plusscript < matlab.unittest.TestCase
     methods(Test)
         function Test1(testCase)
-            actSolution = PLUS;
-            expSolution = 1;
+            actSolution = PLUS(1);
+            expSolution = 3;
             testCase.verifyEqual(actSolution,expSolution)
         end
         function Test2(testCase)
-            actSolution = PLUS;
+            actSolution = PLUS(2);
             expSolution = 3;
             testCase.verifyEqual(actSolution,expSolution)
         end
         function Test3(testCase)
-            actSolution = PLUS;
-            expSolution = 5;
+            actSolution = PLUS(3);
+            expSolution = 3;
             testCase.verifyEqual(actSolution,expSolution)
         end
     end
 end
-        
