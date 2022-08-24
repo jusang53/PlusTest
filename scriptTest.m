4f3360a45f3702ec8bbeaadb75c87c18cd189951
jusang53 <98146496+jusang53@users.noreply.github.com>
2022-08-25 01:10:33 +0900

    Update scriptTest.m

--- scriptTest.m
+++ scriptTest.m
@@ -2,10 +2,5 @@ clc;
 for n = 1:3
     Testing = "Test" + n;
     Testing = convertStringsToChars(Testing);
-%     suite = [suite; Testing];
-%     runtests(Plusscript,Testing);
-    
-    actSolution = PLUS(n);
-    expSolution = 3;
-    assert(actSolution==expSolution)
+    runtests(Plusscript,Testing);
 end
